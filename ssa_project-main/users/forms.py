271c9b3ba508815418310ca7ce62a5da8e587d64
Lenndogg123 <lboemi@parrastu.catholic.edu.au>
2024-12-09 17:59:31 +1100

    final

---
+++ ssa_project-main/users/forms.py
@@ -0,0 +1,43 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+
+
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(max_length=30, required=True)
+    surname = forms.CharField(max_length=30, required=True)
+    nickname = forms.CharField(max_length=30, required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2', 'first_name', 'surname', 'nickname']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['surname']       
+        if commit:
+            user.save()
+            profile = user.profile
+            profile.first_name = self.cleaned_data['first_name']
+            profile.surname = self.cleaned_data['surname']
+            profile.nickname = self.cleaned_data['nickname']
+            profile.save()
+        return user
+    
+class UserTopUp(forms.Form):
+    amount = forms.DecimalField(
+        max_digits=10, 
+        decimal_places=2,
+        min_value=0.01,
+        label="Enter Amount",
+        widget=forms.NumberInput(attrs={'placeholder': '0.01', 'class': 'form-control'})
+    )
+
+
+
+    
